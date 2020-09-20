541b38e8bcf6c1254917ff1dfaa28977a65168f3
Shoham Dey Sarkar <shohamdeysarkar@gmail.com>
2020-09-21 00:39:57 +0530

    Adding Hand Detection

--- Hoverdraw.py
+++ Hoverdraw.py
@@ -9,17 +9,30 @@ def top_shapes_color(frame):
     cv.line(frame,(440,0),(650,100),(0,0,255),2,cv.BORDER_ISOLATED)
     cv.line(frame,(650,0),(440,100),(0,0,255),2,cv.BORDER_ISOLATED)
 
+#detecting the Hand
+def hand_detection(frame):
+    kernel = np.ones((5,5),np.uint8)
+    imgHSV=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+    lower_skin = np.array([0,2,40], dtype=np.uint8)
+    upper_skin = np.array([173,49,127], dtype=np.uint8)
+    mask=cv.inRange(imgHSV,lower_skin,upper_skin)
+    mask = cv.dilate(mask,kernel,iterations = 4)
+    mask = cv.GaussianBlur(mask,(5,5),100) 
+    cv.imshow("Hand",mask)
+
 video=cv.VideoCapture(0)
 
 #Setting Screen Values 
-video.set(3,660)
-video.set(4,612)
+video.set(3,1060)
+video.set(4,812)
 
 #Video Loop
 while True:
     success,frame=video.read()
     top_shapes_color(frame)
+    hand_detection(frame)
     cv.imshow("Hover Draw",frame)
     if cv.waitKey(1) == ord('q'):
         break
 video.release()
+# cv.distroyAllWindows()
