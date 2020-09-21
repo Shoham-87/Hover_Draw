eaa55503aecc75d0f800696447e302c67e509567
Shoham Dey Sarkar <shohamdeysarkar@gmail.com>
2020-09-21 12:56:56 +0530

    On Screen Draw With Red Color

--- Hoverdraw.py
+++ Hoverdraw.py
@@ -11,14 +11,37 @@ def top_shapes_color(frame):
 
 #detecting the Hand
 def hand_detection(frame):
-    kernel = np.ones((5,5),np.uint8)
+    newPoints=[]
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
     imgHSV=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
-    lower_skin = np.array([0,2,40], dtype=np.uint8)
-    upper_skin = np.array([173,49,127], dtype=np.uint8)
+    lower_skin = np.array([98,23,0], dtype=np.uint8)
+    upper_skin = np.array([179,255,142], dtype=np.uint8)
     mask=cv.inRange(imgHSV,lower_skin,upper_skin)
     mask = cv.dilate(mask,kernel,iterations = 4)
     mask = cv.GaussianBlur(mask,(5,5),100) 
-    cv.imshow("Hand",mask)
+    x,y=getContours(mask)
+    cv.circle(imgResult,(x,y),10,(0,0,255),cv.FILLED,cv.BORDER_ISOLATED)
+    if x!=0 and y>=100:
+        newPoints.append([x,y])
+    return newPoints
+
+#getting contours around the hand
+def getContours(frame):
+    contours,hierarchy = cv.findContours(frame,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
+    x,y,w,h = 0,0,0,0
+    for cnt in contours:
+        area = cv.contourArea(cnt)
+        if area>500:
+            cv.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            peri = cv.arcLength(cnt,True)
+            approx = cv.approxPolyDP(cnt,0.02*peri,True)
+            x, y, w, h = cv.boundingRect(approx)
+    return x+h//2,y
+
+#To Draw on the Screen
+def DrawOnScreen(myPoints):
+    for point in myPoints:
+        cv.circle(imgResult, (point[0], point[1]), 10, (0,0,255), cv.FILLED,cv.BORDER_ISOLATED)
 
 video=cv.VideoCapture(0)
 
@@ -26,12 +49,19 @@ video=cv.VideoCapture(0)
 video.set(3,1060)
 video.set(4,812)
 
+myPoints =  [] 
 #Video Loop
 while True:
     success,frame=video.read()
-    top_shapes_color(frame)
-    hand_detection(frame)
-    cv.imshow("Hover Draw",frame)
+    imgResult=frame.copy()
+    top_shapes_color(imgResult)
+    newPoints=hand_detection(frame)
+    if len(newPoints)!=0:
+        for x in newPoints:
+            myPoints.append(x)
+    if len(myPoints)!=0:
+        DrawOnScreen(myPoints)
+    cv.imshow("Hover Draw",imgResult)
     if cv.waitKey(1) == ord('q'):
         break
 video.release()
